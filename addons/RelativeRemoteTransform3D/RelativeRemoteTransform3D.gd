03fe687ec2e9c7e11053cf31be352b46a8e74a15
Nolan Woods <innovate.invent@gmail.com>
2024-03-23 17:41:41 -0700

    Refactor RelativeRemoteTransform3D

--- addons/RelativeRemoteTransform3D/RelativeRemoteTransform3D.gd
+++ addons/RelativeRemoteTransform3D/RelativeRemoteTransform3D.gd
@@ -3,30 +3,51 @@
 #class_name RelativeRemoteTransform3D
 extends Node3D
 
+const Common = preload("common.gd")
+const Transform3DObserver = preload("Transform3DObserver.gd")
+
 @export_category("RelativeRemoteTransform3D")
 
 ## [Node3D] to calculate transforms relative to
-@export_node_path("Node3D") var relative_to: NodePath
+@export_node_path("Node3D") var relative_to: NodePath:
+	set(v):
+		relative_to = v
+		force_update_cache()
 var _relative_to: Node3D
 
 ## [Node3D] to apply transform
-@export_node_path("Node3D") var remote: NodePath
+@export_node_path("Node3D") var remote: NodePath:
+	set(v):
+		remote = v
+		force_update_cache()
 var _remote: Node3D
 
 ## Remote is positioned relative to this specified [Node3D]
-@export_node_path("Node3D") var remote_relative_to: NodePath
+@export_node_path("Node3D") var remote_relative_to: NodePath:
+	set(v):
+		remote_relative_to = v
+		force_update_cache()
 var _remote_relative_to: Node3D
 
 @export_group("Flip Axis")
 
 ## Flip the X axis of the transform
-@export var flip_x = false
+@export var flip_x = false:
+	set(v):
+		flip_x = v
+		_update_flip()
 
 ## Flip the Y axis of the transform
-@export var flip_y = false
+@export var flip_y = false:
+	set(v):
+		flip_y = v
+		_update_flip()
 
 ## Flip the Z axis of the transform
-@export var flip_z = false
+@export var flip_z = false:
+	set(v):
+		flip_z = v
+		_update_flip()
 
 @export_group("Update", "update_")
 
@@ -39,43 +60,52 @@ var _remote_relative_to: Node3D
 ## Restrict updating the scale of the remote
 @export var update_scale: bool = true
 
+var flip: Basis
+
+func _update_flip():
+	flip = Basis.from_scale(Vector3i(-1 if flip_x else 1, -1 if flip_y else 1, -1 if flip_z else 1))
+
 ## Force the 
 func force_update_cache():
-	_relative_to = get_node(relative_to)
-	_remote = get_node(remote)
-	_remote_relative_to = get_node(remote_relative_to)
+	if not relative_to.is_empty():
+		_relative_to = get_node_or_null(relative_to)
+		if _relative_to and _relative_to is Transform3DObserver and not _relative_to.moved.is_connected(update):
+			_relative_to.moved.connect(update)
+	if not remote.is_empty():
+		_remote = get_node_or_null(remote)
+	if not remote_relative_to.is_empty():
+		_remote_relative_to = get_node_or_null(remote_relative_to)
+		if _remote_relative_to and _relative_to is Transform3DObserver and not _remote_relative_to.moved.is_connected(update):
+			_remote_relative_to.moved.connect(update)
 	update()
 
 func _ready():
 	force_update_cache()
+	_update_flip()
 
 func _enter_tree():
-	set_notify_transform(true);
+	set_notify_transform(true)
+	
+func _exit_tree():
+	if _relative_to is Transform3DObserver and _relative_to.moved.is_connected(update):
+		_relative_to.moved.disconnect(update)
 
 func _notification(what):
 	if what == NOTIFICATION_TRANSFORM_CHANGED:
 		update()
 
 func update():
-	var xform := get_global_transform()
-	var relative := _relative_to.get_global_transform()
-	var remote_xform := _remote.get_global_transform()
+	if not (_remote and _relative_to and _remote_relative_to):
+		return
 	var remote_relative := _remote_relative_to.get_global_transform()
-	var flip := Basis.from_scale(Vector3i(-1 if flip_x else 1, -1 if flip_y else 1, -1 if flip_z else 1))
-	var remote_basis := remote_relative.basis.orthonormalized() * flip * relative.basis.orthonormalized().inverse()
+	var relative := Common.to_relative_transform(
+		_relative_to.get_global_transform() if _relative_to is Transform3DObserver and _relative_to.reverse_relationship else get_global_transform(), 
+		get_global_transform() if _relative_to is Transform3DObserver and _relative_to.reverse_relationship else _relative_to.get_global_transform(),
+		)
+	remote_relative.basis = remote_relative.basis.orthonormalized() * flip
 
-	if update_position:
-		remote_xform.origin = (remote_basis * (xform.origin - relative.origin)) + remote_relative.origin
-		
-	remote_basis = remote_basis * xform.basis.orthonormalized()
-	if update_rotation && update_scale:
-		remote_xform.basis = remote_basis
-	elif update_rotation || update_scale:
-		var kept_scale := remote_basis if update_scale else remote_xform.basis.orthonormalized()
-		var kept_rotation := remote_basis if update_rotation else remote_xform.basis.orthonormalized()
-		remote_xform.basis = Basis.from_euler(kept_rotation.get_euler()) * Basis.from_scale(kept_scale.get_scale())
+	var new_xform := Common.from_realtive_transform(relative, remote_relative)
+	if update_rotation || update_scale:
+		new_xform.basis = new_xform.basis.orthonormalized()
 
-	_remote.set_global_transform(remote_xform)
-		
-	
-	
+	_remote.set_global_transform(Common.update_transform(_remote.get_global_transform(), new_xform, update_position, update_rotation, update_scale))
